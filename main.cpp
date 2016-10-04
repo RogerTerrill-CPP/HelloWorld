4b9be7f8777358efef988809dc0d270690c216ce
Roger Terrill <roger.terrill@antaira.com>
2016-10-04 10:51:01 -0700

    Initial

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+        cout << "My name is Roger Chicas-Terrill\nAnd I LOVE CSCI 123F very much.\n";
+}
+
+/*
+Name        :   Homework1a.cpp
+Arguments   :   double tips, and double subtotal
+Return      :   0
+Definition  :   This function displays a string.
+*/
